0812312c99bf736bc2737eba3dbec8b42ae06565
dengzl <dengzl@thinkive.com>
2022-01-14 17:25:33 +0800

    feat: 配置别名

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import App from './App.md'
+import App from '@/App.md'
 
 const version = ref('Vue3')
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
+import  { resolve } from 'path'
 import Markdown from 'vite-plugin-md'
 
 export default defineConfig({
@@ -9,4 +10,11 @@ export default defineConfig({
         }),
         Markdown(),
     ],
+    resolve:{
+        alias:{
+            '@':resolve(__dirname,'src'),
+            '#':resolve(__dirname,'src/components'),
+            '~':resolve(__dirname,'src/assets'),
+        },
+    },
 })
