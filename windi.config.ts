2b81b351df1ae86e9e1cb2fcbf9d39e63efa0532
ranxinstart <ranxinstart@gmail.com>
2022-01-21 18:33:11 +0800

    feat: 修改windi间距

--- windi.config.ts
+++ windi.config.ts
@@ -1,6 +1,10 @@
+import { createSpacing } from './src/utils/create'
 import { defineConfig } from 'windicss/helpers'
 
 export default defineConfig({
+    theme: {
+        spacing: createSpacing(),
+    },
     extract: {
         include: ['src/**/*.{vue,html,jsx,tsx}'],
         exclude: ['node_modules', '.git'],
