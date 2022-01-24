d98f8003c4d3c622239d9766049f214fc676e79a
ranxinstart <ranxinstart@gmail.com>
2022-01-24 13:55:51 +0800

    feat: 添加dayjs

--- src/views/auth/login/index.vue
+++ src/views/auth/login/index.vue
@@ -1,24 +1,40 @@
 <template>
   <div class="login-container">
-    <section>
-      <div>16:21</div>
-      <div class="flex gap-1 h-20">
-        <div>2022/1/21</div>
-        <div>星期五</div>
+    <section class="current-time">
+      <div class="text-7xl mb-10">
+        {{ currentTime[2] }}
+      </div>
+      <div class="flex text-4xl">
+        <div>{{ currentTime[0] }}</div>
+        <div>，</div>
+        <div>{{ currentTime[1] }}</div>
       </div>
     </section>
   </div>
 </template>
 <script lang="ts" setup>
-import { createSpacing } from '@/utils/create'
-console.log(
-    createSpacing(),
-)
+import { onMounted,onUnmounted,ref } from 'vue'
+import { useDateNow, useNow } from 'vue-composable'
+import dayjs from 'dayjs'
+const currentTime = ref<string[]>([])
+const { remove } = useNow({
+    timeFn(){
+        currentTime.value = dayjs().format('YYYY/MM/DD|dddd|HH:mm:ss').split('|')
+        return 0
+    },
+})
+onUnmounted(remove)
 </script>
 
 <style scoped>
 .login-container {
   width: 100vw;
   height: 100vh;
+}
+
+.current-time {
+  position: fixed;
+  left: 5%;
+  bottom: 5%;
 }
 </style>
