7fdcc2d317fb826c26fb7d868a1bb4cc1cb5e8d5
ranxinstart <ranxinstart@gmail.com>
2022-01-26 18:23:16 +0800

    feat: 添加展开收起视图

---
+++ src/components/ApplicationCard.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="w-40 h-40">
+    00
+  </section>
+</template>
+<script lang="ts" setup>
+
+</script>
--- src/layout/NavLayout.vue
+++ src/layout/NavLayout.vue
@@ -1,3 +1,36 @@
 <template>
+  <section
+    class="menu"
+    :class="[openMenu ? 'on' : 'off']"
+    @click="openMenu = !openMenu"
+  >
+    <IconButton v-if="!openMenu" />
+    <section v-else>
+      <ApplicationCard />
+    </section>
+  </section>
   <router-view />
 </template>
+<script lang="ts">
+import IconButton from '~icons/bytesize/chevron-bottom'
+import ApplicationCard from '#/ApplicationCard'
+</script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+const openMenu = ref(false)
+
+</script>
+<style scoped>
+.menu{
+  @apply fixed inset-x-0 transition-all m-auto shadow flex;
+  &.off {
+    @apply w-28 h-22 justify-center rounded-b-md hover:bg-gray-50 cursor-pointer top-0;
+  }
+  &.on {
+    @apply w-4/5 p-10 justify-center rounded-md top-20;
+  }
+}
+
+</style>
